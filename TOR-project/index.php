b210a041271824995cb51c4330f4e38327056cc8
yelenary <yelenary@gmail.com>
2018-03-06 21:17:26 -0500

    Created zip + changed web page for DB pull

--- TOR-project/index.php
+++ TOR-project/index.php
@@ -3,6 +3,15 @@
   <title>TOR PHP WEB site - assignment for Trinimbus</title>
  </head>
  <body>
+	<h1>Simple DB query - Find user based on location</h1>
+	<form method="post">
+		<label for="location">Location</label>
+		<input type="text" id="location" name="location">
+		<input type="submit" name="submit" value="View Results">
+	</form>
+
+<a href="index.php">Back to home</a>
+</body>
  <?php 
  echo '<p>TOR-Hello Trinimbus!</p>'; 
  echo '<br>Local IP: '.$_SERVER['SERVER_ADDR'];
